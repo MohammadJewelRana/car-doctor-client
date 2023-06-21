927f67099a05d29cb7951ecdfed0aebdaa109e8d
MohammadJewelRana <js.rana0326@gmail.com>
2023-06-21 11:39:26 +0600

    full project using react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {   } from 'react'
+ 
+
+
+function App() {
+ 
+
+  return (
+    <>
+       <h1 className='text-center'>Jewel</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer';
+import Header from '../Pages/Shared/Header';
+
+const MainLayout = () => {
+    return (
+        <div>
+            
+         <Header></Header>
+          <Outlet></Outlet>
+          <Footer></Footer>
+
+
+        </div>
+    );
+};
+
+export default MainLayout;
